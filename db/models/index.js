fd68aafc5cccc42ac1e51fed0e731f88ac97d22d
Daniel Wasserman <daniel.h.wasserman@gmail.com>
2019-10-17 18:08:02 -0400

    chore: setting up rudimentary db

---
+++ db/models/index.js
@@ -0,0 +1,10 @@
+// const User = require('./user');
+// const Stock = require('./stock');
+
+// User.belongsToMany(Stock, { through: 'User_Stock' });
+// Stock.belongsToMany(User, { through: 'User_Stock' });
+
+// module.exports = {
+//   User,
+//   Stock,
+// };
