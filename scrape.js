39ed3b272f7f75712ca1758e0210eca081704eac
Daniel Wasserman <daniel.h.wasserman@gmail.com>
2019-10-17 16:05:04 -0400

    feat: scrape.js grabs all individual book links, navigates to each book, grabs most of the data for each

--- scrape.js
+++ scrape.js
@@ -1,23 +1,53 @@
 const puppeteer = require('puppeteer');
 
-let scrape = async () => {
+let scrapeBooks = async () => {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   await page.goto('http://books.toscrape.com/');
   await page.waitFor(1000);
-  await page.click(
-    'body > div > div > div > div > section > div:nth-child(2) > ol > li:nth-child(1) > article > div.image_container > a'
-  );
-  const result = await page.evaluate(() => {
-    let title = document.querySelector('h1').innerText;
-    let price = document.querySelector('.price_color').innerText;
-    return { title, price };
-  });
-  browser.close();
 
-  return result;
+  let nextButton = await page.evaluate(() => {
+    let button = document.getElementsByClassName('next')[0];
+    let buttonLink = button.getElementsByTagName('a')[0].href;
+    return buttonLink;
+  });
+
+  let allBookLinks = await page.evaluate(() => {
+    let books = document.getElementsByClassName('image_container');
+    let booksArray = [...books];
+    let bookLinks = booksArray.map(book => {
+      return book.firstElementChild.href;
+    });
+    return bookLinks;
+  });
+
+  for (let i = 0; i < allBookLinks.length; i++) {
+    let singleBookLink = allBookLinks[i];
+    await page.goto(singleBookLink);
+    const singleBookInfo = await page.evaluate(() => {
+      const genre = document
+        .getElementsByClassName('breadcrumb')[0]
+        .getElementsByTagName('li')[2].innerText;
+
+      const title = document.getElementsByTagName('h1')[0].innerText;
+      const rating = document.getElementsByTagName('p')[2].className;
+      const price = document.getElementsByClassName('price_color')[0].innerText;
+      const availability = document.getElementsByClassName(
+        'instock availability'
+      )[0].innerText;
+      const productDescription = document.getElementsByTagName('p')[3]
+        .innerText;
+
+      console.log(rating);
+    });
+    await page.waitFor(1000);
+  }
+
+  await page.goto(nextButton);
+
+  // browser.close();
 };
 
-scrape().then(value => {
+scrapeBooks().then(value => {
   console.log(value);
 });
